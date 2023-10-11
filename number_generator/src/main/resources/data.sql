a4c48dc3e60e1b0c69c87f74fc89b9fe2d94ea0f
Vladimir Baganov <99076440+DEBAGanov@users.noreply.github.com>
2023-10-11 23:48:11 +0300

    Spring Boot. REST-сервис, выдающий автомб. номера.

--- number_generator/src/main/resources/data.sql
+++ number_generator/src/main/resources/data.sql
@@ -1,3 +1,5 @@
 INSERT INTO car_numbers (number) VALUES ('A001AA');
 INSERT INTO car_numbers (number) VALUES ('A002AA');
 INSERT INTO car_numbers (number) VALUES ('A003AA');
+
+INSERT INTO car_numbers (number) VALUES ('BB003BB');
